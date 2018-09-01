975d881ec6ef1e434ab7fb48651beef1f6dfb1dd
Samanvay Karambhe <samanvaykarambhe@gmail.com>
2018-09-01 20:14:04 +1000

    Add test for url creation

--- test/test_asx_scraper.py
+++ test/test_asx_scraper.py
@@ -8,3 +8,8 @@ class AsxScrapperTest(unittest.TestCase):
         url = f'https://www.asx.com.au/asx/markets/equityPrices.do?by=asxCodes&asxCodes={stock_code}'
         self.assertTrue(isinstance(float(asx_scraper.price(url)), float))
 
+    def test_url(self):
+        expected = 'helloworld.com/VAS'
+        result = asx_scraper.url('helloworld.com/', 'vas')
+        self.assertEqual(expected, result)
+
