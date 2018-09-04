9f5f8ed96b6ca35ab7a0f1a7c7426f14ac1fbe42
Samanvay Karambhe <samanvaykarambhe@gmail.com>
2018-09-04 19:16:16 +1000

    Test for stock price update in google sheets

--- test/test_stock_price_scraper.py
+++ test/test_stock_price_scraper.py
@@ -2,6 +2,7 @@ import unittest
 import bin.stock_price_scraper as stock_price_scraper
 import gspread
 import ujson
+from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
 class StockPriceScraperTest(unittest.TestCase):
@@ -17,20 +18,35 @@ class StockPriceScraperTest(unittest.TestCase):
         self.assertTrue(isinstance(float(stock_price_scraper.price(url)), float))
 
     def test_url(self):
-        expected = 'helloworld.com/VAS'
-        result = stock_price_scraper.url('helloworld.com/', 'vas')
+        expected = 'http://samanvaykarambhe.com/VAS'
+        result = stock_price_scraper.url('http://samanvaykarambhe.com/', 'VAS')
         self.assertEqual(expected, result)
 
-    # def test_stock_prices(self):
-    #     result = stock_price_scraper.StockPriceScraper(base_url='', stock_codes=['VAS', 'VTS'], google_sheet='', client_secret='', test=True).stock_prices()
-    #     for code, price in result.items():
-    #         self.assertTrue(isinstance(float(price), float))
+    def test_stock_prices(self):
+        result = stock_price_scraper.StockPriceScraper(base_url='http://samanvaykarambhe.com/', stock_codes=['VAS', 'VTS'], google_sheet='', client_secret='', test=True).stock_prices()
+        for code, price in result.items():
+            self.assertTrue(isinstance(float(price), float))
 
-    # def test_cell_update(self):
-    #     scope = ['https://spreadsheets.google.com/feeds']
-    #     credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials/asx-scraper-credentials.json', scope)
-    #     gc = gspread.authorize(credentials)
-    #     sh = gc.create('test_spreadsheet')
-    #     worksheet = sh.get_worksheet(0)
-    #     stock_price_scraper.update_cell(worksheet, 'A1', 'Hello')
-    #     self.assertEqual('Hello', worksheet.acell('B1').value)
+    def test_cell_update(self):
+        gc = stock_price_scraper.client('credentials/asx-scraper-credentials.json')
+        sh = gc.open('test')
+        worksheet = sh.get_worksheet(0)
+        stock_price_scraper.update_cell(worksheet, 'A1', '')
+        stock_price_scraper.update_cell(worksheet, 'A1', 'Hello')
+        self.assertEqual('Hello', worksheet.acell('A1').value)
+
+    def test_stock_price_update(self):
+        stock_price_scraper.StockPriceScraper(
+            base_url='http://samanvaykarambhe.com/',
+            stock_codes=['VAS', 'VTS'],
+            google_sheet='test',
+            client_secret='credentials/asx-scraper-credentials.json',
+            test=False
+        ).insert_prices()
+        gc = stock_price_scraper.client('credentials/asx-scraper-credentials.json')
+        sh = gc.open('test')
+        worksheet = sh.worksheet(f'{datetime.today().strftime("%Y-%m-%d")}')
+        self.assertEqual(
+            ['VAS', '80.45', 'VTS', '102.1'],
+            [worksheet.acell('A1').value, worksheet.acell('A2').value, worksheet.acell('B1').value, worksheet.acell('B2').value]
+        )
